330a7eac97081bfc215cb9f1062369c2884ae654
admin <admin@mocambos.net>
2012-09-13 18:11:17 -0400

    Script para inicialização do portal

---
+++ script/install-django-env.sh
@@ -0,0 +1,9 @@
+# Pacotes necessarios 
+sudo apt-get install libldap2-dev libsasl2-dev libssl-dev python-virtualenv python2.6 python2.6-dev
+virtualenv -p /usr/bin/python2.6 --no-site-packages --distribute VirtualEnvs/Mocambos_Portal
+source ~/VirtualEnvs/Mocambos_Portal/bin/activate
+pip install -e requirements.txt
+
+# # Pacotes necessarios para django-filer
+# sudo apt-get install libfreetype6-dev liblcms1-dev libjpeg8-dev
+# pip install django-filer
